4b0b4e98f29aed7f45ba9d22fe61e1c245c3003f
girish9660 <123969938+girish9660@users.noreply.github.com>
2024-05-29 15:58:06 +0530

    added requirements for Sign Up and Profile

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,15 @@
+/**
+ *  Should have following fields
+ * 1. First name
+ * 2. Middle name
+ * 3. Last name
+ * 4. Phone number
+ * 5. Email
+ * 6. Address Line 1
+ * 7. Address Line 2
+ * 8. Address Line 3 (place holder : land mark)
+ * 10. City
+ * 11. Pincode
+ * 12. State
+ * 13 . Save button
+ */
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,10 @@
+/**
+ * it should have the following fields
+ * 
+ * 1. Email
+ * 2. user name
+ * 3. password
+ * 4. confirm password
+ * 5. Sign up
+ * 
+ */
